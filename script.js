560ca2a9e833fd55684b0c3eaa1a6f2016e6f460
vikash rajbhar <vikasrajbhar1012@gmail.com>
2025-08-30 22:15:32 +0530

    Updated CSS for responsive design

--- script.js
+++ script.js
@@ -34,5 +34,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.querySelector(".nav ul");
+
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+
 
 
