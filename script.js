fe44dd844ed4689b5f74373349c3ab294871a4e6
vikash rajbhar <vikasrajbhar1012@gmail.com>
2025-08-30 15:13:03 +0530

    Added my website project

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let allbtns = document.querySelectorAll('.btn,.btn1');
+      console.log("Total buttons found:", allbtns.length);
+    allbtns.forEach((button) => {
+        button.addEventListener('click', () => {
+            alert("your order is sussecfully conform:");
+        });
+    });
+
+});
+
+    let seeBtn = document.querySelector('.see-btn');
+    console.log("See All button found:", seeBtn);
+    seeBtn.addEventListener('click', () => {
+        alert("Redirecting to all collection page...");
+    });
+    document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault(); 
+
+    
+      const name = document.querySelector("input[type='text']").value.trim();
+      const email = document.querySelector("input[type='email']").value.trim();
+      const message = document.querySelector("textarea").value.trim();
+
+      
+      if (name && email && message) {
+        alert("Your form is filled successfully!");
+        form.reset(); 
+      } else {
+        alert("Please fill all fields before submitting.");
+      }
+    });
+  });
+  
+
+
